d35eddc1255b79a120954d638ba5461962639ac4
Aline Carvalho <afcarvalho1@latam.stefanini.com.br>
2019-12-17 16:35:55 -0300

    Atualização para deploy

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+var app = express();
+var distDir = __dirname + '/dist/';
+app.use(express.static(distDir));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 var port = process.env.PORT || 8000
 app.listen(port)
